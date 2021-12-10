fd7c07440b9fcdd4f6c2ccf115847858c213b1e5
syadav2410 <95910436+syadav2410@users.noreply.github.com>
2021-12-10 21:32:08 +0530

    Create pushoperator.c

---
+++ pushoperator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 4
+
+int stack_arr[MAX];
+int top = -1;
+
+void push(int data)
+{
+    if(top == MAX - 1){
+		printf("Stack overflow");
+		return;
+			}	
+	top = top + 1;
+	stack_arr[top] = data;
+}
+
+int main()
+{
+	push(1);
+	push(2);
+	push(3);
+	push(4);
+	push(5);
+	return 0;
+}
